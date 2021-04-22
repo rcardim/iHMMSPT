1548aa0b92f67ea8e3336b2d3874716db4532c33
Rebeca Cardim Falcao <rebecardim@gmail.com>
2021-04-22 13:24:37 -0700

    Update main.m

--- main.m
+++ main.m
@@ -1,20 +1,19 @@
+addpath( [pwd '\sampler_SRC'])
 %% directories
 dirIN=pwd;
-addpath( [dirIN '\sampler_SRC'])
+%% Data Load
+%data has to be displacements (1-D); if 2-D or 3-D: use new dimensions as chains
+%multiple chains for >1 dimension
+fname='Displacements-4.mat';
+load(fullfile(dirIN,fname));
+handles.x=DataX(:,1)'; %DataX comes from 'Displacements-4.mat' 
+error=0; %experimental error
 %% Iteration steps
 % Number of iterations and percentage of burn-in steps.
 r_total=5000; % total number of steps 
 per=0.15; % percentage of steps on burn-in phase
 r_burnin=per*r_total;
 r_max=(1-per)*r_total;
-%% Data Load
-%data has to be displacements (1-D); if 2-D or 3-D: use new dimensions as chains
-%multiple chains for >1 dimension
-%displacement=sqrt(x^2+y^2+z^2). 
-fname='Displacements-4.mat';
-load(fullfile(dirIN,fname));
-handles.x=DataX(:,1)'; %data 
-error=0;%experimental error
 %% Hyperparameters and other variables for the algorithm
 opts.K_init = 10; %initial number of states
 opts.a = 1; %hyperparameter alpha of the Dirichlet Process (transition probabilities concentration)
