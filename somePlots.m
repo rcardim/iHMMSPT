3948529edbbd6b20d3ef3f4e9e3c694f3fd991f4
Rebeca Cardim Falcao <rebecardim@gmail.com>
2021-04-22 13:29:13 -0700

    Update somePlots.m

--- somePlots.m
+++ somePlots.m
@@ -1,9 +1,12 @@
 %% Script to plot some graphs of states and diffusion
 %% load Results
 load('results-3.mat')
+%% setting parameters of experimental data
 tau=0.001; %tau = sampling time = 1/frame_rate (1/s)
 pixel_size=1; % size of 1 pixel in microns (for simulated data is 1)
+%% the number of burn-ins
 burn_in=750;% burn-in steps
+
 %% Plot states chain
 fig1=figure
 plot(chain_final.K,'LineWidth', 2)
@@ -15,32 +18,23 @@ xlabel('Iterations')
 ylabel('Number of States')
 set(gca,'FontSize',20)
 saveas(fig1,'statesGraph.png')
+
 %% Boxplot graph of diffusions
 variance=[];
-prob=[];
 count=0;
 statesEstimated=mode(chain_final.K(burn_in:end));
-Transitions=zeros(statesEstimated,statesEstimated);
 for i=burn_in:length(chain_final.F)
     iteration=chain_final.F{i};
-    iteration_P=chain_final.P{i};
     if size(iteration,1)==statesEstimated
         count=count+1;
-        Transitions=Transitions+iteration_P(:,1:statesEstimated);
         variance=[variance iteration(:,2)];
-        prob=[prob iteration(:,3)];
     end
 end
-T=Transitions./count;
-D1=(1./(2*variance*tau))*pixel_size^2;
-Dm=mean(D1,2);
-pm=mean(prob,2);
-res1=[Dm; pm]';
-resf=sortrows(res1,1);
-D2=sortrows(D1,1);
-D2=D2';
+diffusivities=(1./(2*variance*tau))*pixel_size^2;
+sorted_diffusivities=sortrows(diffusivities,1);
+sorted_diffusivities=sorted_diffusivities';
 fig2 = figure
-boxplot(D2,'whisker',1000)
+boxplot(sorted_diffusivities,'whisker',1000)
 hold on
 xlabel('State')
 ylabel('Diffusion [\mum^2/s]')
@@ -48,6 +42,7 @@ title('Results 3') %title of graph
 set(gca,'FontSize',20)
 hold off
 saveas(fig2,'BoxplotDiffusionState.png')
+
 %% Scatter Plot of diffusion values for every iteration
 fig3=figure
 for i=2:2:length(chain_final.F)
